bb1a0b7a5d027a1afaa41cfabf16f512fe599618
AstridAmery <astridamery99@gmail.com>
2026-07-23 22:55:33 +0100

    tkinter integration

--- PasswordChecker/main.py
+++ PasswordChecker/main.py
@@ -2,17 +2,31 @@ import string
 import re
 from getpass import getpass
 import tkinter as tk
+from tkinter import ttk
 
-tk.Tk()
-password = getpass()
+window = tk.Tk()
+
+label = tk.Label(text="Enter your password: ")
+label.pack()
+window.geometry("200x100")
+window.title("Password Strength Checker")
+
+password_entry = ttk.Entry(window, show="*")
+password_entry.pack(pady=5)
+password_entry.focus()
+
+def check_password():
+    if (len(password_entry.get()) >= 10 and len(password_entry.get()) <= 20
+        and re.search("[a-z]", password_entry.get())
+        and re.search("[A-Z]", password_entry.get())
+        and re.search("[0-9]", password_entry.get())
+        and re.search("[_@$£!,&*]", password_entry.get())):
+        label.config(text= "This password is strong.")
+    else:
+        label.config(text="This password is weak.")
 
 
-if (len(password) >= 10
-    and re.search("[a-z]", password)
-    and re.search("[A-Z]", password)
-    and re.search("[0-9]", password)
-    and re.search("[_@$£!,&*]", password)
-    and not re.search("[\s]", password)):
-    print("This password is strong.")
-else:
-    print("This password is weak.")
+button = ttk.Button(window, text="Check Password", command=lambda: check_password())
+button.pack(pady=5)
+
+window.mainloop()
